4a099f6bbf1b02bfa344a016d26fdcbbd6229045
Daniel Egan <djegan@casablanca.local>
2017-03-25 11:02:18 +1030

    Added a bit more

--- src/main/java/com/jirvan/SnapshotMarker.java
+++ src/main/java/com/jirvan/SnapshotMarker.java
@@ -4,4 +4,13 @@ public class SnapshotMarker {
 
     public static boolean testingOnly_mustBeRemoved = true;
 
+    public static boolean testingOnly_mustBeRemoved(String message) {
+        System.out.printf("\n%s\n\n", message);
+        return true;
+    }
+
+    public static boolean testingOnly_mustBeRemoved() {
+        return true;
+    }
+
 }
