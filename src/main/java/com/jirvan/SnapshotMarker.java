8d5f15d5fe2b83a8977bd2b4f4238050d925e43a
Daniel Egan <djegan@casablanca.local>
2017-06-07 19:50:10 +0930

    Checkpoint

--- src/main/java/com/jirvan/SnapshotMarker.java
+++ src/main/java/com/jirvan/SnapshotMarker.java
@@ -1,16 +1,26 @@
 package com.jirvan;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SnapshotMarker {
 
     public static boolean suppressMessages = false;
     public static boolean surroundMessagesWithBlankLines = true;
+    public static Set<String> displayedMessages = new TreeSet<>();
 
     public static boolean testingOnly_mustBeRemoved(String message) {
+        return testingOnly_mustBeRemoved(message, true);
+    }
+
+    private static boolean testingOnly_mustBeRemoved(String message, boolean displayMessageOnceOnly) {
         if (!suppressMessages) {
-            if (surroundMessagesWithBlankLines) {
-                System.out.printf("\n%s\n\n", message);
-            } else {
-                System.out.printf("%s\n", message);
+            if (!displayMessageOnceOnly || displayedMessages.add(message)) {
+                if (surroundMessagesWithBlankLines) {
+                    System.out.printf("\n%s\n\n", message);
+                } else {
+                    System.out.printf("%s\n", message);
+                }
             }
         }
         return true;
