b1b21e9361048c5d2946e9eda8940b7bb8d809a4
Daniel Egan <djegan@casablanca.local>
2017-06-16 18:21:08 +0930

    Checkpoint

--- src/main/java/com/jirvan/SnapshotMarker.java
+++ src/main/java/com/jirvan/SnapshotMarker.java
@@ -1,5 +1,7 @@
 package com.jirvan;
 
+import com.jirvan.dates.Millisecond;
+
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,6 +10,24 @@ public class SnapshotMarker {
     public static boolean suppressMessages = false;
     public static boolean surroundMessagesWithBlankLines = true;
     public static Set<String> displayedMessages = new TreeSet<>();
+
+    public static boolean sleep_mustBeRemoved(int millis) {
+        return sleep_mustBeRemoved(millis, null);
+    }
+
+    public static boolean sleep_mustBeRemoved(int millis, String message) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return message != null ? testingOnly_mustBeRemoved(message) : true;
+    }
+
+    public static boolean timing_mustBeRemoved(int millis, String message) {
+        System.out.printf("%s: %s\n", new Millisecond().toString(), message);
+        return true;
+    }
 
     public static boolean testingOnly_mustBeRemoved(String message) {
         return testingOnly_mustBeRemoved(message, true);
